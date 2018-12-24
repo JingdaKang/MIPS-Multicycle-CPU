a57f2c09e86a992cc9d5e2ca3440d0ec31e5e5da
MIPS CPU计组实验 <37934630+JingdaKang@users.noreply.github.com>
2018-12-24 10:14:42 +0800

    Add files via upload

---
+++ Define/ALUOP_Define.v
@@ -0,0 +1,41 @@
+//nop
+`define ALUOP_NOP 6'd0
+
+//arithmetic
+
+`define ALUOP_ADD 6'd1
+`define ALUOP_ADDU 6'd1
+
+`define ALUOP_SUB 6'd2
+`define ALUOP_SUBU 6'd2
+
+`define ALUOP_DIV 6'd3
+`define ALUOP_DIVU 6'd4
+
+`define ALUOP_MULT 6'd5
+`define ALUOP_MULTU 6'd6
+
+//logical
+
+//shift
+`define ALUOP_SLL 6'd7
+`define ALUOP_SLLV 6'd7
+`define ALUOP_SRA 6'd8
+`define ALUOP_SRAV 6'd8
+`define ALUOP_SRL 6'd9
+`define ALUOP_SRLV 6'd9
+
+//logic
+`define ALUOP_AND 6'd10
+`define ALUOP_OR 6'd11
+`define ALUOP_XOR 6'd12
+`define ALUOP_NOR 6'd13
+
+//compare
+`define ALUOP_SLT 6'd14
+`define ALUOP_SLTU 6'd15
+`define ALUOP_SGT 6'd16
+`define ALUOP_SGTU 6'd17
+
+//low->high
+`define ALUOP_LUI 6'd18
---
+++ Define/DEBUG_Define.v
@@ -0,0 +1,6 @@
+//MIPS_TB
+`define DEBUG
+
+//datapath
+`define DM_DEBUG
+`define RF_DEBUG
